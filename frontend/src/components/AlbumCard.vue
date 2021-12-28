e5f1263ed1694f53ae94e01a4143ecf3d192080c
Adrià Sala <adriasala124@gmail.com>
2021-12-28 18:22:14 +0100

    Added icons to AlbumCard

--- frontend/src/components/AlbumCard.vue
+++ frontend/src/components/AlbumCard.vue
@@ -2,9 +2,10 @@
   <div class="card my-2 mx-2 p-0 border-0" :id="'album-' + albumName" style="width: 18rem; max-height: 25rem">
     <img class="card-img-top" :src="imageUrl" alt="Album cover image" />
     <div class="card-body">
-      <p class="card-title text-truncate">{{ albumName }}</p>
-      <p class="card-text text-truncate">
-        <small class="text-muted">{{ artistName }}</small>
+      <p class="card-title text-truncate"><i class="fa fa-music mx-2"></i>{{ albumName }}</p>
+      <p class="card-text text-truncate text-muted">
+        <i class="fa fa-user-alt mx-2"></i>
+        <small>{{ artistName }}</small>
       </p>
     </div>
   </div>
