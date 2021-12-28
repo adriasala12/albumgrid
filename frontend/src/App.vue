bb7314437e3095a81299c6f547bf30353bdd7e10
Adrià Sala <adriasala124@gmail.com>
2021-12-28 01:51:34 +0100

    Added filtering and minor improvements

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,21 @@
   <div class="fill">
     <!-- NAVBAR -->
     <nav class="navbar static-top navbar-dark bg-dark">
-      <div class="containter">
+      <div class="mx-auto">
         <span class="navbar-brand align-bottom">
           <span class="px-2 text-light align-text-bottom"
             ><b-icon-grid3x3-gap-fill></b-icon-grid3x3-gap-fill
           ></span>
+          <span class="px-2 text-light align-text-bottom"
+            ><b-icon-music-note></b-icon-music-note
+          ></span>
           Album Grid
-        </span>
-        <span class="navbar-text d-none d-md-inline align-text-bottom">
-          Find the albums of your favourite artists!
+          <span class="px-2 text-light align-text-bottom"
+            ><b-icon-music-note></b-icon-music-note
+          ></span>
+          <span class="px-2 text-light align-text-bottom"
+            ><b-icon-grid3x3-gap-fill></b-icon-grid3x3-gap-fill
+          ></span>
         </span>
       </div>
     </nav>
@@ -18,9 +24,11 @@
     <!-- SEARCH SECTION -->
     <section id="search" class="">
       <div class="container align-items-center">
-        <div class="row mt-4">
+        <div class="row mt-5">
           <div class="col">
-            <h1 class="text-light">Find your albums:</h1>
+            <h2 class="text-light">
+              Find the albums of your favourite artists!
+            </h2>
           </div>
         </div>
         <div class="row mt-4">
@@ -30,7 +38,7 @@
               type="text"
               placeholder="Search by artist name"
               aria-describedby="search-button"
-              v-model="searchterm"
+              v-model="searchTerm"
               @keyup.enter="getAlbums"
             />
           </div>
@@ -50,17 +58,56 @@
       </div>
     </section>
 
+    <hr v-show="albums.length" class="my-5" style="color: white" />
+
     <!-- RESULTS SECTION -->
     <section id="results" class="mt-5">
       <div v-if="loading" class="d-flex justify-content-center">
         <div class="spinner-border text-light" role="status"></div>
       </div>
 
-      <div class="container">
+      <div
+        v-if="showEmpty"
+        class="d-flex mt-5 justify-content-center text-light"
+      >
+        <h4>No results found for this artist</h4>
+      </div>
+
+      <div v-if="albums.length" class="container">
+        <div class="row justify-content-end my-4">
+          <div class="col-5">
+            <div class="input-group">
+              <input
+                class="form-control round"
+                type="text"
+                placeholder="Filter by album name"
+                v-model="filterTerm"
+                v-on:keyup="filterAlbums"
+              />
+
+              <button
+                type="button"
+                class="btn bg-transparent align-text-top"
+                style="margin-left: -40px; z-index: 100"
+                v-on:click="clearFilter"
+              >
+                <b-icon-x-circle-fill></b-icon-x-circle-fill>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div
+          v-if="filterTerm && !showAlbums.length"
+          class="d-flex mt-5 justify-content-center text-light"
+        >
+          <h4>No results found that match your filter</h4>
+        </div>
+
         <div class="row justify-content-center justify-content-md-between">
           <div
             class="card my-2 mx-2"
-            v-for="album in albums"
+            v-for="album in showAlbums"
             :key="album.collectionName"
             style="width: 18rem"
           >
@@ -91,26 +138,60 @@ export default {
     return {
       loading: false,
       albums: [],
-      searchterm: "",
+      showAlbums: [],
+      searchTerm: "",
+      filterTerm: "",
+      showEmpty: false,
     };
   },
   methods: {
     getAlbums: async function () {
-      this.loading = true;
-      axios
-        .get(`http://localhost:3000/${this.searchterm.replace(" ", "+")}`)
-        .then((response) => {
-          response.data.forEach(
-            (x) =>
-              (x.artworkUrl100 = x.artworkUrl100.replace("100x100", "300x300"))
-          );
-          this.albums = response.data;
-          this.loading = false;
-        })
-        .catch((err) => console.log(err));
+      if (this.searchTerm !== "") {
+        this.loading = true;
+        axios
+          .get(`http://localhost:3000/${this.searchTerm.replace(" ", "+")}`)
+          .then((response) => {
+            if (response.data.length === 0) {
+              this.loading = false;
+              this.albums = this.showAlbums = [];
+              this.showEmpty = true;
+              return;
+            } else {
+              this.showEmpty = false;
+              response.data.forEach(
+                (x) =>
+                  (x.artworkUrl100 = x.artworkUrl100.replace(
+                    "100x100",
+                    "300x300"
+                  ))
+              );
+              this.albums = this.showAlbums = response.data;
+              this.loading = false;
+            }
+          })
+          .catch((err) => console.log(err));
+      }
+      else {
+        this.albums = this.showAlbums = []; 
+      }
+    },
+    filterAlbums: function () {
+      this.showAlbums = this.albums.filter((x) =>
+        x.collectionName.toLowerCase().includes(this.filterTerm.toLowerCase())
+      );
+    },
+    clearFilter: function () {
+      this.filterTerm = "";
+      this.showAlbums = this.albums;
     },
   },
   components: {},
 };
 </script>
 
+<style>
+.round {
+  border-top-right-radius: 5px !important;
+  border-bottom-right-radius: 5px !important;
+}
+</style>
